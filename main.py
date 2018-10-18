73940a15af797eac937fff38089ecc55ee0bfde9
Jorick <defraine.jorick@gmail.com>
2018-10-18 10:03:15 +0200

    ouvrir un fichier .csv avec panda création d'un processing.py definition de test.py

--- main.py
+++ main.py
@@ -2,10 +2,6 @@
 si vous avez bien pull, ce message s'affiche.
 '''
 
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+from processing import test
 
-df = pd.read_csv('day.csv')
-
-print(df.head())
+print(test())
---
+++ processing.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+def test():
+    df = pd.read_csv('day.csv')
+    return df.head()
