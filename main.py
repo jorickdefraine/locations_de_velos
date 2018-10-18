44b0f41522bd5790025c4601b5ad6d37b2118650
Jorick <defraine.jorick@gmail.com>
2018-10-18 09:55:37 +0200

    ouvrir un fichier .csv avec panda

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 '''
 si vous avez bien pull, ce message s'affiche.
 '''
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('day.csv')
+
+print(df.head())
