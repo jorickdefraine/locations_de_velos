cefff64087fe0d0e8ff7a84485fc5dafa49d70c6
Jorick <defraine.jorick@gmail.com>
2018-10-18 10:15:00 +0200

    deplacement des data dans un dossier "data"

--- processing.py
+++ processing.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import data
 
 def OpenData():
-    df = pd.read_csv('day.csv')
+    df = pd.read_csv('data/day.csv')
     return df.head()
