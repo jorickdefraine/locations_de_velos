f6a0dfa34eb142b262b227836f383b9015f2baf7
Jorick Defraine <defraine.jorick@gmail.com>
2018-11-07 11:53:10 +0100

    ajout de la fonction afiche_corr(DataFrame,size) qui affiche la matrice de correlation de taille size des données DataFrame.

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 si vous avez bien pull, ce message s'affiche.
 '''
 
-from processing import OpenData
+from processing import OpenData, affiche_corr
 
 print(OpenData())
+affiche_corr(OpenData(),16)
+
--- processing.py
+++ processing.py
@@ -6,3 +6,16 @@ import data
 def OpenData():
     df = pd.read_csv('data/day.csv')
     return df.head()
+def affiche_corr(DataFrame,size):
+    '''
+    fonction qui affiche une matrice de correlation  pour chaque pair de colonne des données.
+
+    Paramètres:
+        DataFrames: pandas DataFrame
+        size: taille verticale et horizontale de la matrice de correlation'''
+    correlation = DataFrame.corr(method='pearson')
+    fig, ax = plt.subplots(figsize=(size, size))
+    ax.matshow(correlation)
+    plt.xticks(range(len(correlation.columns)), correlation.columns)
+    plt.yticks(range(len(correlation.columns)), correlation.columns)
+    plt.show()
