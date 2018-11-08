4c67ec2cd0977992fa94ad751859228827199db0
Asella <asella.cho@gmail.com>
2018-11-08 10:15:29 +0100

    Mis à jour

--- type_deep_learning_test.py
+++ type_deep_learning_test.py
@@ -4,6 +4,13 @@ Created on Thu Nov  8 09:13:00 2018
 
 @author: Charpak 14
 """
+
+import pandas
+import matplotlib.pyplot as plt
+dataset = pandas.read_csv(r"data\day.csv", usecols=[1], engine='python', skipfooter=3)
+plt.plot(dataset)
+plt.show()
+
 import numpy
 import matplotlib.pyplot as plt
 import pandas
