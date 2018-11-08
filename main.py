a55aee7f5c045cbae69439212132db0b39e88aad
Jorick Defraine <defraine.jorick@gmail.com>
2018-11-08 08:59:27 +0100

    ajout de la fonction feature importance (en cours) plus ajout de data

--- main.py
+++ main.py
@@ -2,9 +2,13 @@
 si vous avez bien pull, ce message s'affiche.
 '''
 
-from processing import OpenData, affiche_corr, affiche_hm
+from processing import OpenData, affiche_corr, affiche_hm, feature_imp, firstLine
 
 print(OpenData())
-affiche_corr(OpenData(), 16)
+#affiche_corr(OpenData(), 16)
 affiche_hm(OpenData())
+print("Première ligne")
 
+
+#print(firstLine(0))
+#feature_imp(OpenData(), firstLine(0))
