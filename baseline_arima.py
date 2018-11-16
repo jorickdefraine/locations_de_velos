0bd366bb88624afb5cb937177118aae844fa97b1
Jorick Defraine <defraine.jorick@gmail.com>
2018-11-16 17:25:44 +0100

    ajout de la fonction rsmle qui calcule l'erreur de arima()

--- baseline_arima.py
+++ baseline_arima.py
@@ -12,44 +12,52 @@ from type_random_forest import OpenData
 def arima():
     variables = OpenData()
     count = variables['cnt']
-
     lnprice = np.log(count)
 
     plt.plot(lnprice)
     plt.show()
-    acf_1 = acf(lnprice)[1:20]
+    acf_1 = acf(lnprice)[1:360]
     plt.plot(acf_1)
     plt.show()
     test_df = pandas.DataFrame([acf_1]).T
-    test_df.columns = ['Pandas Autocorrelation']
+    test_df.columns = ['Autocorrelation par pandas']
     test_df.index += 1
     test_df.plot(kind='bar')
-    pacf_1 = pacf(lnprice)[1:20]
+    pacf_1 = pacf(lnprice)[1:360]
     plt.plot(pacf_1)
     plt.show()
     test_df = pandas.DataFrame([pacf_1]).T
-    test_df.columns = ['Pandas Partial Autocorrelation']
+    test_df.columns = ['Autocorrelation partiel par pandas']
     test_df.index += 1
     test_df.plot(kind='bar')
     result = ts.adfuller(lnprice, 1)
 
     lnprice_diff = lnprice - lnprice.shift()
     diff = lnprice_diff.dropna()
-    acf_1_diff = acf(diff)[1:20]
+    acf_1_diff = acf(diff)[1:360]
     test_df = pandas.DataFrame([acf_1_diff]).T
-    test_df.columns = ['First Difference Autocorrelation']
+    test_df.columns = ['Autocorrelation des différences premières']
     test_df.index += 1
     test_df.plot(kind='bar')
-    pacf_1_diff = pacf(diff)[1:20]
+    pacf_1_diff = pacf(diff)[1:360]
     plt.plot(pacf_1_diff)
     plt.show()
 
-    price_matrix=lnprice.as_matrix()
-    model = ARIMA(price_matrix, order=(0,1,0))
+    price_matrix = lnprice.as_matrix()
+    model = ARIMA(price_matrix, order=(0, 1, 0))
     model_fit = model.fit(disp=0)
     print(model_fit.summary())
 
-    predictions = model_fit.predict(122, 127, typ='levels')
+    predictions = model_fit.predict(1, 731, typ='levels')
     print(predictions)
     predictionsadjusted = np.exp(predictions)
-    print(predictionsadjusted)
+    return predictionsadjusted,count
+
+
+def rmsle(predict_cnt, actual_cnt):
+    for i in range(1,731):
+        somme = (np.log(predict_cnt[i] + 1) - np.log(actual_cnt[i] + 1)) ** 2
+    print("rsmle")
+    rmsle = np.sqrt((1 / 731) * somme)
+    print(rmsle)
+
--- main.py
+++ main.py
@@ -3,8 +3,9 @@ si vous avez bien pull, ce message s'affiche.
 '''
 
 from type_random_forest import OpenData, affiche_corr
-from baseline_arima import arima
+from baseline_arima import arima, rmsle
 print(OpenData())
 
 affiche_corr(OpenData())
 arima()
+rmsle(arima()[0],arima()[1])
