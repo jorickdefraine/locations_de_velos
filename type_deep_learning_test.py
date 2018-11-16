cb7ac97accc0c7537c5ac66bf8d9745fff2879d3
Asella <asella.cho@gmail.com>
2018-11-16 17:30:24 +0100

    transformer les dates en datetime

--- type_deep_learning_test.py
+++ type_deep_learning_test.py
@@ -4,19 +4,31 @@ Created on Thu Nov  8 09:13:00 2018
 
 @author: Charpak 14
 """
-
-import pandas
-import matplotlib.pyplot as plt
-dataset = pandas.read_csv(r"data\day.csv", usecols=[1], engine='python', skipfooter=3)
-plt.plot(dataset)
-plt.show()
-
 import numpy
-import matplotlib.pyplot as plt
 import pandas
-import math
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
+import matplotlib.pyplot as plt
+#from keras.models import Sequential
+#from keras.layers import Dense
+#from keras.layers import LSTM
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
+
+#################
+dataset = pandas.read_csv(r"data\day.csv") #je donne un nom de variable à dataframe
+
+print(dataset.dtypes) #affiche le type des variables dans les données
+
+"""
+#permet de dessiner les graphes intitulés avec les variables
+for col in dataset.columns:
+    plt.plot(dataset.index, dataset[col])
+    plt.title(col)
+    plt.figure()
+plt.show()
+"""
+#permet de transformer les dates en datetime
+dataset['dteday'] = pandas.to_datetime(dataset['dteday'])
+
+#index affiche correctement que les dates sont datetime
+dataset.index = dataset['dteday']
+print(dataset.dtypes)
