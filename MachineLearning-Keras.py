59041c9789e02fab191532c2393be2148b31954d
Leadersheepy <marine.kazemi@gmail.com>
2018-11-29 00:29:28 +0100

    Ajout d'aide pour Keras

--- MachineLearning-Keras.py
+++ MachineLearning-Keras.py
@@ -1,1 +1,3 @@
-
+"""http://cedric.cnam.fr/vertigo/Cours/ml2/tpDeepLearning3.html"""
+"""https://www.actuia.com/keras/"""
+"""https://medium.com/tensorflow/predicting-the-price-of-wine-with-the-keras-functional-api-and-tensorflow-a95d1c2c1b03"""
