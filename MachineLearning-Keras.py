e0a1b093d9c401a2ea3f30622b9efa640054bbde
Jirock <34106142+Jirock@users.noreply.github.com>
2018-11-29 09:18:26 +0100

    delete MachineLearning-Keras.py

--- MachineLearning-Keras.py
+++
@@ -1,3 +0,0 @@
-"""http://cedric.cnam.fr/vertigo/Cours/ml2/tpDeepLearning3.html"""
-"""https://www.actuia.com/keras/"""
-"""https://medium.com/tensorflow/predicting-the-price-of-wine-with-the-keras-functional-api-and-tensorflow-a95d1c2c1b03"""
