a9cbcdc6f10ea44d11487b728e2ebfb13827fdeb
Jorick Defraine <defraine.jorick@gmail.com>
2018-12-04 00:16:06 +0100

    Merge branch 'master' of C:\Users\Jorick\IdeaProjects\locations_de_velos with conflicts.

--- process.py
+++ process.py
@@ -5,13 +5,7 @@ Fichier principal
 from baseline_arima import arima
 from matrice_de_correlation import affiche_corr
 from tools import openData, rmsle
-from type_machine_learning_python import machineLearning
 
-"""
 affiche_corr(openData())
 arima()
 rmsle(arima()[0], arima()[1])
-
-machineLearning()
-"""
-
