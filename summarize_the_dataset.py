0fe2ddabf269d0d8ec43376481692a3f6739e962
Asella <asella.cho@gmail.com>
2018-12-07 15:02:12 +0100

    summarize dataset

---
+++ summarize_the_dataset.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  8 09:12:45 2018
+
+@author: charpak4.21
+"""
+
+from tools import openData
+
+import pandas
+from pandas.plotting import scatter_matrix
+import matplotlib.pyplot as plt
+
+from sklearn import model_selection
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+
+
+#Pour nos données
+dataset = openData()
+print(dataset.dtypes)
+
+print(dataset)
+print(dataset.columns)
+
+#3.1 Dimensions of Dataset
+print(dataset.shape)
+
+#3.2 Peek at the Data
+print(dataset.head(20))
+
+#3.3 Statistical Summary
+print(dataset.describe())
+
+#3.4 Class Distribution
+print(dataset.groupby('holiday').size())
+print(dataset.columns)
+print(dataset.groupby('season').size())
+print(dataset.groupby('weathersit').size())
+
+#4. Data Visualization
+#4.1 Univariate Plots
+dataset.plot(kind='box', subplots=True, layout=(4,4), sharex=False, sharey=False)
+plt.show()
+
+dataset.hist()
+plt.show()
+
+#4.2 Multivariate Plots
+scatter_matrix(dataset)
+plt.show()
