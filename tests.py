84be00fb6ceef1285d682413bbd3e505c672c2c5
Jorick Defraine <defraine.jorick@gmail.com>
2018-12-21 06:22:16 +0100

    fix tests

--- tests.py
+++ tests.py
@@ -6,10 +6,9 @@ Ne pas oublier d'importer chaque fonction et fichier utilisé (une fois suffit).
 from tools import rmsle, openData
 import numpy as np
 
-l = list(range(10))
-l2 = list(range(1, 11))
-
 
 def test():
+    l = list(range(10))
+    l2 = list(range(1, 11))
     test_RMSLE1 = np.testing.assert_almost_equal(rmsle(openData()['cnt'], openData()['cnt']), 0.000, decimal=3)
     test_RMSLE2 = np.testing.assert_almost_equal(rmsle(l, l2), 0.2977, decimal=3)
